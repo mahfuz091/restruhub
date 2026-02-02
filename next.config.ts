91befc8f4275b40e721e6a85af6ef574c5caf283
Md. Rezoyan Mahfuz <121441920+mahfuz091@users.noreply.github.com>
2026-02-02 20:48:42 +0600

    Add custom headers to Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [{ key: "X-Robots-Tag", value: "noindex, nofollow" }],
+      },
+    ];
+  },
   images: {
     domains: ["res.cloudinary.com"],
     remotePatterns: [
