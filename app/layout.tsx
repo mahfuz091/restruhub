2725a373edefc5abeb9f24b0f592a8a04df60d1e
mahfuz091 <engmahfuz14@gmail.com>
2026-03-18 16:47:28 +0600

    remove google code

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,10 +41,7 @@ export const metadata: Metadata = {
     canonical: "/",
   },
 
-  verification: {
-    google: "Org5r9Jt5ST9xlH0pyLhNXxXsBINQDebx81TK885cpQ",
-  },
-
+  
   openGraph: {
     type: "website",
     siteName: "RestruHub",
@@ -70,34 +67,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google Tag Manager */}
-        {/* <Script
-          id="gtm"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${GTM_ID}');`,
-          }}
-        /> */}
-        {/* End Google Tag Manager */}
+      
       </head>
 
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} antialiased bg-[#ffffff]`}
       >
-        {/* Google Tag Manager (noscript) */}
-        {/* <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript> */}
-        {/* End Google Tag Manager (noscript) */}
+    
 
          <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-MCVQZY096H"
